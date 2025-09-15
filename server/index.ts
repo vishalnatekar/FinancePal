be77d1b43e9dd867d689db767f9a6a831535d466
Vishal.Natekar <natekar.vishal@gmail.com>
2025-09-15 09:50:45 +0200

    fixed

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,18 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { log } from "./vite";
 import fs from "fs";
 import path from "path";
+
+// Simple logger used in both dev and prod
+function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+  console.log(`${formattedTime} [${source}] ${message}`);
+}
 
 const app = express();
 app.use(express.json());
